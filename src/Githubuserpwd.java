342bcfecc6d8d0bf1845a9e157b49ce57d10d86c
dell <gauravdevgd2000@gmail.com>
2023-01-24 11:03:55 -0800

    This is my username and password

---
+++ src/Githubuserpwd.java
@@ -0,0 +1,8 @@
+public class Githubuserpwd {
+    public static void main(String[] args){
+        String userName= "Gauravdev2000";
+        System.out.println("Username is : " +userName);
+        String passWord= "Gaurav@745507";
+        System.out.println("Password is : " +passWord);
+    }
+}
