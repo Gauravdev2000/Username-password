ad75cd205d29cdd469d58916d810d96d1415d4ae
dell <gauravdevgd2000@gmail.com>
2023-01-25 18:38:48 -0800

    Square and cube

---
+++ src/SquareandCube1.java
@@ -0,0 +1,8 @@
+public class SquareandCube1 {
+    public static void main(String[] args){
+        int a= 23;
+        int multi= a*a*a;
+        System.out.println("Multi of a : " +multi);
+
+    }
+}
